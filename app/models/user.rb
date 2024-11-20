33a791442c821f2f10862374ac63976252b62e34
BronwenM <bronwen.marchant@gmail.com>
2024-11-20 14:25:57 -0800

    Added testing and model validation to handle cases where a user has mixed case email or extra spaces on either end

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,13 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, confirmation: true, length: {minimum: 6}
   validates :password_confirmation, presence: true
+  before_save :downcase_email
 
   def self.authenticate_with_credentials(email, password)
-    User.find_by(email: email)&.authenticate(password)
+    User.find_by(email: email.strip.downcase)&.authenticate(password)
+  end
+
+  def downcase_email
+    self.email.downcase!
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,45 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  
   describe 'Users' do
-    it 'creates a valid user' do
+    before do
       @user = User.create(name: "Marty", email: "marty@mcfly.tech", password: "delorean", password_confirmation: "delorean")
+      @user_2 = User.create(name: "Marty", email: "marty@mcfly.tech", password: "delorean", password_confirmation: "delorean")
+      @user_3 = User.create(name: "Marty", email: "marty@mcfly.tech", password: "delorean", password_confirmation: "delorean") 
+
+    end
+
+    it 'creates a valid user' do
+      # @user = User.create(name: "Marty", email: "marty@mcfly.tech", password: "delorean", password_confirmation: "delorean")
       
       expect(@user).to be_valid      
     end
     
     it 'should have an email' do
-      @user = User.create(name: "Marty", email: nil, password: "delorean", password_confirmation: "delorean")
-      
+      # @user = User.create(name: "Marty", email: nil, password: "delorean", password_confirmation: "delorean")
+      @user.email = nil
       expect(@user).to_not be_valid
     end
 
     it 'should have a unique email' do
-      @user_1 = User.create(name: "Marty", email: "marty@mcfly.tech", password: "delorean", password_confirmation: "delorean")
-      @user_2 = User.create(name: "Marty", email: "marty@mcfly.tech", password: "delorean", password_confirmation: "delorean")
-      @user_3 = User.create(name: "Marty", email: "MARTY@mcfly.tech", password: "delorean", password_confirmation: "delorean") 
+      @user_2.email = "marty@mcfly.tech"
+      @user_3.email = "MARTY@mcfly.tech"
 
-      expect(@user_1).to be_valid
+      expect(@user).to be_valid
       expect(@user_2).to_not be_valid
       expect(@user_3).to_not be_valid
     end
 
     it 'should be created with password and confirmation' do 
-      @user_1 = User.create(name: "Marty", email: nil, password: nil, password_confirmation: "delorean")
-      @user_2 = User.create(name: "Marty", email: nil, password: "delorean", password_confirmation: nil)
-      
-      expect(@user_1).to_not be_valid
+      @user.password_confirmation = nil
+      @user_2.password_confirmation = nil
+
+      expect(@user).to_not be_valid
       expect(@user_2).to_not be_valid
     end
 
     it 'password should match password_confirmation' do
-      @user = User.create(name: "Marty", email: "marty@mcfly.tech", password: "Delorean", password_confirmation: "delorean")
-      
+      @user.password = "Delorean"
+      @user.password_confirmation = "delorean"
+
       expect(@user).to_not be_valid
     end
 
     it 'password should have at least 6 characters' do
-      @user = User.create(name: "Marty", email: "marty@mcfly.tech", password: "del", password_confirmation: "del")
+      @user.password = "del"
       
       expect(@user).to_not be_valid
     end
@@ -48,11 +56,19 @@ RSpec.describe User, type: :model do
 
   describe '.authenticate_with_credentials' do
     before do
-      @user = User.create(name: "Marty", email: "marty@mcfly.tech", password: "delorean", password_confirmation: "delorean")
+      @user = User.create(name: "Marty", email: "mArty@mcfly.tech", password: "delorean", password_confirmation: "delorean")
     end
 
     it 'should log in a valid user' do
       expect(User.authenticate_with_credentials("marty@mcfly.tech", "delorean")).to eq(@user)
+    end
+
+    it 'should log in a valid user where the email has extra spaces' do
+      expect(User.authenticate_with_credentials("  marty@mcfly.tech  ", "delorean")).to eq(@user)
+    end
+
+    it 'should log in a valid user where the email has mixed case' do
+      expect(User.authenticate_with_credentials("  maRTy@mcfly.tech  ", "delorean")).to eq(@user)
     end
   end
 
