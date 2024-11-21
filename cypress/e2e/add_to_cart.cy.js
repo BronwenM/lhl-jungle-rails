40623fc525116f33aade2ff4195a1b9e97d7e59b
BronwenM <bronwen.marchant@gmail.com>
2024-11-20 16:20:37 -0800

    Added test and fix to prevent product with no stock to be added to cart

--- cypress/e2e/add_to_cart.cy.js
+++ cypress/e2e/add_to_cart.cy.js
@@ -1,20 +1,33 @@
-describe("Add to Card", () => {
-  beforeEach(() => {
+describe("Add to Cart", () => {
+  it("should add 1 in stock item to cart", () => {
     cy.visit("/");
+
     cy.get(".products article [alt='Scented Blade']")
       .click();
 
     cy.contains("Scented Blade").should("exist")
-  });
-
-  it("should add 1 item to cart", () => {
-    cy.get("a").contains("My Cart").contains(0);
+    
+    cy.get("a").contains("My Cart").contains('0');
 
     cy.contains("button", "Add")
       .click();
     
-    cy.get("a").contains("My Cart").contains(1);
-    cy.get("a").contains("My Cart").contains(0).should("not.be.true");
+    cy.get("a").contains("My Cart").contains('1');
+    cy.get("a").contains("My Cart").contains('0').should("not.be.true");
+  });
+
+  it("should NOT add 1 out-of-stock item to cart", () => {
+    cy.visit("/");
+
+    cy.get(".products article [alt='Giant Tea']")
+      .click();
+
+    cy.contains("Giant Tea").should("exist")
+
+    cy.contains("button", "Add")
+      .click();
+    
+    cy.get("a").contains("My Cart").contains('0');
   });
   
 })
